4fccccc4a51b54b5b168a1949bbe7bd89b572b42
Jon Ibarreche <jon.ibarreche@opendeusto.es>
2025-02-27 15:58:25 +0100

    Commit inicial del portfolio personal

---
+++ gitprofile.config.ts
@@ -0,0 +1,152 @@
+// gitprofile.config.ts
+
+const CONFIG = {
+  github: {
+    username: 'jonibarreche', // Your GitHub org/user name. (This is the only required config)
+  },
+  /**
+   * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
+   * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
+   * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
+   */
+  base: '/',
+  projects: {
+    github: {
+      display: true, // Display GitHub projects?
+      header: 'Github Projects',
+      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      automatic: {
+        sortBy: 'stars', // Sort projects by 'stars' or 'updated'
+        limit: 8, // How many projects to display.
+        exclude: {
+          forks: false, // Forked projects will not be displayed if set to true.
+          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        },
+      },
+      manual: {
+        // Properties for manually specifying projects
+        projects: [], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+      },
+    },
+    external: {
+      header: 'My Projects',
+      // To hide the `External Projects` section, keep it empty.
+      projects: [],
+    },
+  },
+  profile: {
+    image: '', // Puedes añadir un enlace a tu imagen
+    role: 'Desarrollador de Software',
+    bio: 'Desarrollador de software apasionado por la tecnología y el aprendizaje continuo.',
+  },
+  seo: {
+    title: 'Joni Barreche',
+    description: 'Joni Barreche - Portfolio Personal',
+    imageURL: '',
+  },
+  social: {
+    linkedin: '',
+    twitter: '',
+    mastodon: '',
+    researchGate: '',
+    facebook: '',
+    instagram: '',
+    reddit: '',
+    threads: '',
+    youtube: '',
+    udemy: '',
+    dribbble: '',
+    behance: '',
+    medium: '',
+    dev: '',
+    stackoverflow: '',
+    leetcode: '',
+    skype: '',
+    telegram: '',
+    website: 'https://jonibarreche.github.io',
+    phone: '',
+    email: '',
+  },
+  resume: {
+    fileUrl: '',
+  },
+  skills: [
+    'JavaScript',
+    'React.js',
+    'Node.js',
+    'HTML',
+    'CSS',
+    'Git',
+  ],
+  experiences: [],
+  certifications: [],
+  awards: [],
+  educations: [],
+  publications: [],
+  themeConfig: {
+    defaultTheme: 'dim',
+
+    // Hides the switch in the navbar
+    // Useful if you want to support a single color mode
+    disableSwitch: true,
+
+    // Should use the prefers-color-scheme media-query,
+    // using user system preferences, instead of the hardcoded defaultTheme
+    respectPrefersColorScheme: false,
+
+    // Display the ring in Profile picture
+    displayAvatarRing: true,
+
+    // Available themes. To remove any theme, exclude from here.
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'dim',
+      'nord',
+      'sunset',
+      'procyon',
+    ],
+
+    // Custom theme, applied to `procyon` theme
+    customTheme: {
+      primary: '#fc055b',
+      secondary: '#219aaf',
+      accent: '#e8d03a',
+      neutral: '#2A2730',
+      'base-100': '#E3E3ED',
+      '--rounded-box': '3rem',
+      '--rounded-btn': '3rem',
+    },
+  },
+
+  enablePWA: true,
+};
+
+export default CONFIG;
